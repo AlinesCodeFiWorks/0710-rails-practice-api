2efcc581be727af78174277e858b5895cb8460d8
Aline Taylor <alineyui.taylor@gmail.com>
2025-07-10 18:50:02 -0500

    SHE LIVES: set up controller, seed file, and models.

--- rails-practice/db/seeds.rb
+++ rails-practice/db/seeds.rb
@@ -1,1 +1,10 @@
-require faker
+require 'faker'
+
+books = []
+10.times do
+  books << Book.create!(
+    title: Faker::Book.title ,
+    author: Faker::Name.name ,
+    year: Faker::Number.within(range:1500..2025)
+  )
+end
